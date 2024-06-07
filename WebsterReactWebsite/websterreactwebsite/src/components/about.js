01d27496500c55975f26767c47dcfde7bca7d08c
Omar Muminovic <65538892+FlashyHydra@users.noreply.github.com>
2024-06-07 16:12:36 +0200

    About Us - Members
    Uld

--- WebsterReactWebsite/websterreactwebsite/src/components/about.js
+++ WebsterReactWebsite/websterreactwebsite/src/components/about.js
@@ -3,6 +3,18 @@ function About() {
     return (
         <header className="App-header">
             <h2 className="introbody-text">About Here</h2>
+            <div className="about-us">
+            <div className="member">
+                <h2>Marco Teran</h2>
+                <p>Software Engineer</p>
+                <p>Marco is a seasoned software engineer with over 10 years of experience in full-stack development...</p>
+            </div>
+            <div className="member">
+                <h2>Omar Muminovic</h2>
+                <p>Product Manager</p>
+                <p>Marco has been leading product teams for 8 years, specializing in agile methodologies and user experience...</p>
+            </div>
+            </div>
         </header>
     );
   }

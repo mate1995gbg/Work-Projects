661108f3ecc1c122b2957bffd45827c602cc7cf4
Omar Muminovic <65538892+FlashyHydra@users.noreply.github.com>
2024-06-07 16:50:38 +0200

    Pfps

--- WebsterReactWebsite/websterreactwebsite/src/components/about.js
+++ WebsterReactWebsite/websterreactwebsite/src/components/about.js
@@ -5,14 +5,16 @@ function About() {
             <h2 className="introbody-text">About Us</h2>
             <div className="about-us">
             <div className="member">
+                <img src="https://media.licdn.com/dms/image/D4E35AQFEWDad5c_mzw/profile-framedphoto-shrink_200_200/0/1711885152021?e=1718377200&v=beta&t=8HL8l1d5RQ8PwJEjVqsjhFbHzp6RKX5czpbNGSYt0LU" className="profile-pic" />
                 <h2>Marco Teran</h2>
                 <p>Software Engineer</p>
-                <p>Marco is a seasoned software engineer with over 10 years of experience in full-stack development...</p>
+                <p>John is a seasoned software engineer with over 10 years of experience in full-stack development...</p>
             </div>
             <div className="member">
+                <img src="https://media.licdn.com/dms/image/D4D03AQFd9VQULF1XFw/profile-displayphoto-shrink_200_200/0/1717666526315?e=1723075200&v=beta&t=YrNmQY-OYfDDhRomQ7wP8jcKwaJtPqd_BXHxp3essBU" className="profile-pic" />
                 <h2>Omar Muminovic</h2>
                 <p>Product Manager</p>
-                <p>Omar has been leading product teams for 8 years, specializing in agile methodologies and user experience...</p>
+                <p>Jane has been leading product teams for 8 years, specializing in agile methodologies and user experience...</p>
             </div>
             </div>
         </header>

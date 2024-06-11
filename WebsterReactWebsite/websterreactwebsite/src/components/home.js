31e60b4c35ccab8d64a14dc6f4232364f5afd285
mate1995gbg <marcogbg@hotmail.com>
2024-06-11 23:46:11 +0200

    changed to portfolio

--- WebsterReactWebsite/websterreactwebsite/src/components/home.js
+++ WebsterReactWebsite/websterreactwebsite/src/components/home.js
@@ -5,19 +5,19 @@ function Home() {
       <div className="App">
         <header className="App-header-home"> 
           <div className="home-welcome-container">
-            <p className='home-intro-text'>Hello! <br></br><br></br>We're a small freelancing <br></br>development studio of two,<br></br>
-             based in Gothenburg, Sweden .</p>
-            <p className="home-intro-emailtext"><br></br>We're available for new projects :</p>
+            <p className='home-intro-text'>Hejsan! <br></br><br></br>Mitt namn är <span className="highlight-cyan">Marco Terán</span>, <br></br>en <span className="highlight-magenta">Systemutvecklare</span><br></br>
+             baserad in Göteborg .</p>
+            <p className="home-intro-emailtext"><br></br>Kontakta mig via:</p>
             <div className="home-email-container">
               <span class="material-symbols-outlined">send
               </span>
-              <p className="home-intro-emailtext">hello@websterdevelopment.se</p>
+              <p className="home-intro-emailtext">marcogbg@hotmail.com</p>
             </div>
           </div>
             <img src={logo} className="App-logo"/>
         </header>
         <div className='App-header-home-lowerpart'>
-          <svg width="100" height="120" viewBox="0 0 100 120" fill="none" xmlns="http://www.w3.org/2000/svg" className='svg-triangles'>
+          <svg width="150" height="180" viewBox="0 0 100 120" fill="none" xmlns="http://www.w3.org/2000/svg" className='svg-triangles'>
             <g id="triangles" clip-path="url(#clip0_1_2)">
             <g id="lightGroup">
             <path id="light1" opacity="0.7" d="M52.5981 38.5C51.4434 40.5 48.5566 40.5 47.4019 38.5L29.2154 7C28.0607 5 29.5041 2.5 31.8135 2.5L68.1865 2.5C70.4959 2.5 71.9393 5 70.7846 7L52.5981 38.5Z"/>
